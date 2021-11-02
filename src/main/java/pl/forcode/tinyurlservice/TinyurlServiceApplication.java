1171f8a4f5fe74b4e5d336370d70d86dfe4f6e59
pscigala <przemyslaw.scigala@gmail.com>
2021-11-02 22:47:19 +0100

    initial commit

---
+++ src/main/java/pl/forcode/tinyurlservice/TinyurlServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.forcode.tinyurlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/forcode/tinyurlservice/TinyurlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.forcode.tinyurlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyurlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
